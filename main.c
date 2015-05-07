33332eff91d95a58fb4a24fbbf131042fcd5d925
andreipurdila <purdila.andrei@gmail.com>
2015-05-07 19:47:42 +0300

    conflict sa fie

--- main.c
+++ main.c
@@ -12,8 +12,26 @@ gerg'ebge54jgvbe
 
 void main(int argc, char** argv)
 {
-
-	printf("Silence!I will kill you!");
+rsgsgsgsgsgsgsgsgsgsgsgfbvrgvbdrgbvdf
+gherdgb
+dfh
+dfghdf
+gsd
+fgfgfgfgfgdgdfgerwtfgsdfvrsegsdf
+fgvzsertgsdfgvrbsergvdf
+dfgertydfvdfgv
+	printfetgery("Sdfgersyhersg
+	dfg
+	dfg
+	erhgr
+	bh
+	gb
+	er
+	gver
+	gvb
+	ergv
+	rer
+	ilence!I will kill you!");
 	return 0;
 
 =======
